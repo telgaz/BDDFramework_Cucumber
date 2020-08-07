c20395b4325f98be5a3ebf146a15be81fb812bb5
Tugba ELGAZ <tugbaelgaz@Tugba-MBP.fios-router.home>
2020-08-07 14:43:35 -0400

    added

--- target/site/cucumber-pretty/report.js
+++ target/site/cucumber-pretty/report.js
@@ -1,73 +1,139 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Messenger.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("LoginPage.feature");
 formatter.feature({
-  "line": 2,
-  "name": "Messenger page feature",
-  "description": "",
-  "id": "messenger-page-feature",
+  "line": 3,
+  "name": "Login Feature",
+  "description": "Given I am on home page",
+  "id": "login-feature",
   "keyword": "Feature",
   "tags": [
     {
       "line": 1,
-      "name": "@web"
+      "name": "@loginPage"
     }
   ]
 });
-formatter.before({
-  "duration": 6567339209,
-  "status": "passed"
-});
-formatter.background({
-  "line": 4,
-  "name": "",
-  "description": "",
-  "type": "background",
-  "keyword": "Background"
-});
-formatter.step({
-  "line": 5,
-  "name": "I am on messenger page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "MessengerPageSD.setMessengerPage()"
-});
-formatter.result({
-  "duration": 2747168005,
-  "status": "passed"
-});
-formatter.scenario({
+formatter.scenarioOutline({
   "line": 7,
-  "name": "Verify invalid login from messenger page",
+  "name": "Verify invalid login for multiple users",
   "description": "",
-  "id": "messenger-page-feature;verify-invalid-login-from-messenger-page",
-  "type": "scenario",
-  "keyword": "Scenario",
+  "id": "login-feature;verify-invalid-login-for-multiple-users",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
   "tags": [
     {
       "line": 6,
-      "name": "@messenger"
+      "name": "@login"
     }
   ]
 });
 formatter.step({
-  "line": 8,
-  "name": "I enter tugba@sample.com into username field on the messenger page",
+  "line": 9,
+  "name": "I enter \u003cusername\u003e into username text field on home screen",
   "keyword": "When "
 });
 formatter.step({
-  "line": 9,
-  "name": "I enter test123 into password field on the messenger page",
-  "keyword": "And "
-});
-formatter.step({
   "line": 10,
-  "name": "I click on login button on messenger page",
+  "name": "I enter \u003cpassword\u003e into password text field on home screen",
   "keyword": "And "
 });
 formatter.step({
   "line": 11,
-  "name": "I verify invalid login message on sign in page",
-  "keyword": "Then "
+  "name": "I click on login button on home screen",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "I verify that invalid credentials",
+  "keyword": "And "
+});
+formatter.examples({
+  "line": 15,
+  "name": "",
+  "description": "",
+  "id": "login-feature;verify-invalid-login-for-multiple-users;",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ],
+      "line": 16,
+      "id": "login-feature;verify-invalid-login-for-multiple-users;;1"
+    },
+    {
+      "cells": [
+        "tugba@sample.com",
+        "test123"
+      ],
+      "line": 17,
+      "id": "login-feature;verify-invalid-login-for-multiple-users;;2"
+    },
+    {
+      "cells": [
+        "jim@gmail.com",
+        "test345"
+      ],
+      "line": 18,
+      "id": "login-feature;verify-invalid-login-for-multiple-users;;3"
+    },
+    {
+      "cells": [
+        "liz@yahoo.com",
+        "1234test"
+      ],
+      "line": 19,
+      "id": "login-feature;verify-invalid-login-for-multiple-users;;4"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 7034921847,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "Verify invalid login for multiple users",
+  "description": "",
+  "id": "login-feature;verify-invalid-login-for-multiple-users;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 6,
+      "name": "@login"
+    },
+    {
+      "line": 1,
+      "name": "@loginPage"
+    }
+  ]
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter tugba@sample.com into username text field on home screen",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I enter test123 into password text field on home screen",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I click on login button on home screen",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "I verify that invalid credentials",
+  "keyword": "And "
 });
 formatter.match({
   "arguments": [
@@ -80,10 +146,10 @@ formatter.match({
       "offset": 30
     }
   ],
-  "location": "MessengerPageSD.enterDataIntoTextField(String,String)"
+  "location": "LoginPageSD.enterDataUserAndPAssField(String,String)"
 });
 formatter.result({
-  "duration": 346588631,
+  "duration": 854823344,
   "status": "passed"
 });
 formatter.match({
@@ -97,28 +163,248 @@ formatter.match({
       "offset": 21
     }
   ],
-  "location": "MessengerPageSD.enterDataIntoTextField(String,String)"
+  "location": "LoginPageSD.enterDataUserAndPAssField(String,String)"
 });
 formatter.result({
-  "duration": 64441153,
+  "duration": 157970314,
   "status": "passed"
 });
 formatter.match({
-  "location": "MessengerPageSD.clickOnSignButton()"
+  "arguments": [
+    {
+      "val": "login button",
+      "offset": 11
+    }
+  ],
+  "location": "LoginPageSD.clickOnButton(String)"
 });
 formatter.result({
-  "duration": 7825786404,
+  "duration": 116828604,
   "status": "passed"
 });
 formatter.match({
-  "location": "MessengerPageSD.verifySignInErrorMessage()"
+  "location": "LoginPageSD.verifyInvalidLogin()"
 });
 formatter.result({
-  "duration": 36038335,
+  "duration": 2190382276,
   "status": "passed"
 });
 formatter.after({
-  "duration": 135814885,
+  "duration": 237242962,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 3787377080,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "Verify invalid login for multiple users",
+  "description": "",
+  "id": "login-feature;verify-invalid-login-for-multiple-users;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 6,
+      "name": "@login"
+    },
+    {
+      "line": 1,
+      "name": "@loginPage"
+    }
+  ]
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter jim@gmail.com into username text field on home screen",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I enter test345 into password text field on home screen",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I click on login button on home screen",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "I verify that invalid credentials",
+  "keyword": "And "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "jim@gmail.com",
+      "offset": 8
+    },
+    {
+      "val": "username",
+      "offset": 27
+    }
+  ],
+  "location": "LoginPageSD.enterDataUserAndPAssField(String,String)"
+});
+formatter.result({
+  "duration": 284553557,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "test345",
+      "offset": 8
+    },
+    {
+      "val": "password",
+      "offset": 21
+    }
+  ],
+  "location": "LoginPageSD.enterDataUserAndPAssField(String,String)"
+});
+formatter.result({
+  "duration": 174889245,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "login button",
+      "offset": 11
+    }
+  ],
+  "location": "LoginPageSD.clickOnButton(String)"
+});
+formatter.result({
+  "duration": 193999182,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginPageSD.verifyInvalidLogin()"
+});
+formatter.result({
+  "duration": 1745997130,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 180629764,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 3602006008,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 19,
+  "name": "Verify invalid login for multiple users",
+  "description": "",
+  "id": "login-feature;verify-invalid-login-for-multiple-users;;4",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 6,
+      "name": "@login"
+    },
+    {
+      "line": 1,
+      "name": "@loginPage"
+    }
+  ]
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter liz@yahoo.com into username text field on home screen",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I enter 1234test into password text field on home screen",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I click on login button on home screen",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "I verify that invalid credentials",
+  "keyword": "And "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "liz@yahoo.com",
+      "offset": 8
+    },
+    {
+      "val": "username",
+      "offset": 27
+    }
+  ],
+  "location": "LoginPageSD.enterDataUserAndPAssField(String,String)"
+});
+formatter.result({
+  "duration": 160531460,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1234test",
+      "offset": 8
+    },
+    {
+      "val": "password",
+      "offset": 22
+    }
+  ],
+  "location": "LoginPageSD.enterDataUserAndPAssField(String,String)"
+});
+formatter.result({
+  "duration": 156835215,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "login button",
+      "offset": 11
+    }
+  ],
+  "location": "LoginPageSD.clickOnButton(String)"
+});
+formatter.result({
+  "duration": 210164988,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginPageSD.verifyInvalidLogin()"
+});
+formatter.result({
+  "duration": 30289261657,
+  "error_message": "org.openqa.selenium.TimeoutException: Expected condition failed: Webdriver waited for 15 seconds nut still could not find the element therefore TimeOutException has been thrown (tried for 15 second(s) with 1000 milliseconds interval)\n\tat org.openqa.selenium.support.ui.FluentWait.timeoutException(FluentWait.java:315)\n\tat org.openqa.selenium.support.ui.FluentWait.until(FluentWait.java:283)\n\tat runnerTest.webPages.ElementUtil.webAction(ElementUtil.java:33)\n\tat runnerTest.webPages.ElementUtil.isElementDisplayed(ElementUtil.java:79)\n\tat runnerTest.webPages.LoginPage.createAccount(LoginPage.java:31)\n\tat stepDefinition.LoginPageSD.verifyInvalidLogin(LoginPageSD.java:50)\n\tat ✽.And I verify that invalid credentials(LoginPage.feature:12)\nCaused by: org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//a[contains(text(), \u0027Create New Account\u0027)]\"}\n  (Session info: chrome\u003d84.0.4147.105)\nFor documentation on this error, please visit: http://seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.14.0\u0027, revision: \u0027aacccce0\u0027, time: \u00272018-08-02T20:19:58.91Z\u0027\nSystem info: host: \u0027Tugba-MBP.fios-router.home\u0027, ip: \u0027fe80:0:0:0:1827:18a3:720b:9004%en0\u0027, os.name: \u0027Mac OS X\u0027, os.arch: \u0027x86_64\u0027, os.version: \u002710.13.6\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 84.0.4147.105, chrome: {chromedriverVersion: 84.0.4147.30 (48b3e868b4cc0..., userDataDir: /var/folders/34/jw2cnhwx42d...}, goog:chromeOptions: {debuggerAddress: localhost:49796}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: MAC, platformName: MAC, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: f177007b95d404a12a6605c68d1c4f50\n*** Element info: {Using\u003dxpath, value\u003d//a[contains(text(), \u0027Create New Account\u0027)]}\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:548)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:322)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:424)\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:314)\n\tat runnerTest.webPages.ElementUtil$1.apply(ElementUtil.java:35)\n\tat runnerTest.webPages.ElementUtil$1.apply(ElementUtil.java:33)\n\tat org.openqa.selenium.support.ui.FluentWait.until(FluentWait.java:260)\n\tat runnerTest.webPages.ElementUtil.webAction(ElementUtil.java:33)\n\tat runnerTest.webPages.ElementUtil.isElementDisplayed(ElementUtil.java:79)\n\tat runnerTest.webPages.LoginPage.createAccount(LoginPage.java:31)\n\tat stepDefinition.LoginPageSD.verifyInvalidLogin(LoginPageSD.java:50)\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\n\tat java.lang.reflect.Method.invoke(Method.java:498)\n\tat cucumber.runtime.Utils$1.call(Utils.java:37)\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:13)\n\tat cucumber.runtime.Utils.invoke(Utils.java:31)\n\tat cucumber.runtime.java.JavaStepDefinition.execute(JavaStepDefinition.java:38)\n\tat cucumber.runtime.StepDefinitionMatch.runStep(StepDefinitionMatch.java:37)\n\tat cucumber.runtime.Runtime.runStep(Runtime.java:299)\n\tat cucumber.runtime.model.StepContainer.runStep(StepContainer.java:44)\n\tat cucumber.runtime.model.StepContainer.runSteps(StepContainer.java:39)\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:44)\n\tat cucumber.runtime.junit.ExecutionUnitRunner.run(ExecutionUnitRunner.java:91)\n\tat org.junit.runners.Suite.runChild(Suite.java:127)\n\tat org.junit.runners.Suite.runChild(Suite.java:26)\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\n\tat cucumber.runtime.junit.ExamplesRunner.run(ExamplesRunner.java:59)\n\tat org.junit.runners.Suite.runChild(Suite.java:127)\n\tat org.junit.runners.Suite.runChild(Suite.java:26)\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\n\tat cucumber.runtime.junit.ScenarioOutlineRunner.run(ScenarioOutlineRunner.java:53)\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:63)\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:18)\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:70)\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:93)\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:37)\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\n\tat cucumber.api.junit.Cucumber.run(Cucumber.java:98)\n\tat org.junit.runner.JUnitCore.run(JUnitCore.java:160)\n\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:68)\n\tat com.intellij.rt.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:33)\n\tat com.intellij.rt.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:230)\n\tat com.intellij.rt.junit.JUnitStarter.main(JUnitStarter.java:58)\n",
+  "status": "failed"
+});
+formatter.embedding("image/png", "embedded0.png");
+formatter.after({
+  "duration": 1277667507,
   "status": "passed"
 });
 });
